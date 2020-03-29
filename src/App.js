aa16c23f751f31b8c5e2464cc341f62a9e99ab7b
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-03-29 18:03:13 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import UserInput from './components/UserInput'
 import ConnectedUsers from './components/Users'
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <UserInput />
         <ConnectedUsers />
-      </div>
+      </div> 
     );
   }
 }
